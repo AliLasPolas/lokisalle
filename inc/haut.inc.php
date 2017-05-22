111eff55f24b9a0070d9bdc8e111ecd254002405
Ali <ali.nizamuddin@lepoles.com>
2017-05-22 12:39:05 +0200

    update base sql

--- inc/haut.inc.php
+++ inc/haut.inc.php
@@ -10,6 +10,39 @@ require_once("inc/init.inc.php");
 	<title>Inscription</title>
 	<link rel="stylesheet" type="text/css" href="">
 	<link rel="stylesheet" type="text/css" href="/lokisalle/inc/css/style.css">
+	<style type="text/css">
+		ul{
+			width: 100vw;
+			height: 50px;
+			background-color: grey;
+		}
+		li{
+			width: 10%;
+			height: : 50px;
+			list-style-type: none;
+			display: inline-block;
+
+		}
+		a{
+			width: 100%;
+			height: 50%;
+			text-decoration: none;
+			color: white;
+		}
+		a:hover{
+			background-color: blue;
+		}
+	</style>
 </head>
 <body>
+<header>
+	<ul>
+		<li><a href="acceuil.php">Accueil</a></li>
+		<li><a href="acceuil.php">Inscription</a></li>
+		<li><a href="acceuil.php">Profil</a></li>
+		<li><a href="acceuil.php">Boutique</a></li>
+		<li><a href="acceuil.php">Se connecter</a></li>
+		<li><a href="acceuil.php">Se deconnecter</a></li>
+	</ul>
+</header>
 <section class="main">
